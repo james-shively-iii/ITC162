067ac703a9b77b51514c63aa0c575b01d2b17e2e
James Shively III <james.shively-iii@seattlecentral.edu>
2019-05-06 22:14:10 -0700

    Adding TipCalculator and Assignment4 to the repo

---
+++ ITC162Assignment4/app/src/main/java/com/example/itc162_assignment4/conversion.java
@@ -0,0 +1,12 @@
+package com.example.itc162_assignment4;
+
+public class conversion {
+    // Celsius
+    public static float celsius(float c) {
+        return ((c * 9)/5 + 32);
+    }
+    // Fahrenheit
+    public static float fahrenheit(float f) {
+        return ((f - 32) * 5/9);
+    }
+}
